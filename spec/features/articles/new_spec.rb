0ff84fe291f5c339b6de72311de1ccea3c39b8d4
Isabelle Villasenor <villasenori@outlook.com>
2020-12-27 07:09:21 -0700

    Add Spec for Creating New Article

---
+++ spec/features/articles/new_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'New Article' do
+  describe 'As A Visitor' do
+    it 'should create a new article via form from index' do
+      visit articles_path
+
+      expect(page).to have_link('Create New Article')
+      click_link 'Create New Article'
+
+      expect(current_path).to eq(new_article_path)
+
+      fill_in :title, with: "Wowie Wowie"
+      fill_in :body, with: "I LeeLuLeeeee"
+      click_button 'Create Article'
+
+      expect(page).to have_content("Wowie Wowie")
+      expect(page).to have_content("I LeeLuLeeeee")
+    end 
+  end
+end
