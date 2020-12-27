51c32e01e7d7c46c75acf3219f83efc918696c9c
Isabelle Villasenor <villasenori@outlook.com>
2020-12-27 07:27:50 -0700

    Add Spec For Destroy Method

--- spec/features/articles/show_spec.rb
+++ spec/features/articles/show_spec.rb
@@ -23,5 +23,16 @@ describe 'Article Show Page' do
 
       expect(current_path).to eq(articles_path)
     end
+
+    it 'should have a link to delete article and reroute to index' do
+      visit "/articles/#{@a1.id}"
+
+      expect(page).to have_link('Delete')
+      click_link 'Delete'
+
+      expect(current_path).to eq(articles_path)
+      expect(page).to have_content(@a2.title)
+      expect(page).to_not have_content(@a1.title)
+    end
   end
 end
