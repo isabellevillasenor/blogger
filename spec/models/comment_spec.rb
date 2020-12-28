3ee018145a0c850a19c7cad839edb7c487357514
Isabelle Villasenor <villasenori@outlook.com>
2020-12-28 09:06:51 -0700

    Add Validations Comment Model Spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
 describe Comment do 
+  describe 'validations' do
+    it { should validate_presence_of(:author_name) }
+    it { should validate_presence_of(:body) }
+  end
 
-  describe "relationships" do
+  describe 'relationships' do
     it { should belong_to(:article) }
   end
-  
 end
