ba0167ab07f85f05d3e8ea3d2009b6afd4df3ce1
Isabelle Villasenor <villasenori@outlook.com>
2020-12-30 09:32:06 -0700

    Add Tag Relationship Through Taggings Spec to Article

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 describe Article do 
-  describe "validations" do
+  describe 'validations' do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:body) }
   end
 
-  describe "relationships" do
+  describe 'relationships' do
     it { should have_many(:comments) }
+    it { should have_many(:tags).through(:taggings) }
   end
 end
