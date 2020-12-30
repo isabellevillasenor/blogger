65bfa9e363cdb4231ed24deac430c2f7b14d2197
Isabelle Villasenor <villasenori@outlook.com>
2020-12-30 09:52:29 -0700

    Add tag_list Model Method to Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,11 @@ class Article < ApplicationRecord
   has_many :taggings
   has_many :tags, through: :taggings
   validates_presence_of :title, :body
+
+  def tag_list
+    self.tags.collect do |t|
+      t.name
+    end.join(", ")
+  end
+  
 end
