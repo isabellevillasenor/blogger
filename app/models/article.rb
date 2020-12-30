1298ac292201895b92aca6344a6adc5a5c18940e
Isabelle Villasenor <villasenori@outlook.com>
2020-12-30 12:35:29 -0700

    Add dependent: :destroy Into Model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
-  has_many :taggings
+  has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
   validates_presence_of :title, :body
 
