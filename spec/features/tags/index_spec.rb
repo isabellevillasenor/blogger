39b9c5a20bb5ba429b6c86c706b10a31d8d89a28
Isabelle Villasenor <villasenori@outlook.com>
2020-12-30 12:33:16 -0700

    Add Delete Spec for Tag Show

--- spec/features/tags/index_spec.rb
+++ spec/features/tags/index_spec.rb
@@ -1,15 +1,20 @@
 require 'rails_helper'
 
 describe 'Tags Index' do
+  before :each do 
+    @t1 = Tag.create!(name: "greetings")
+    @t2 = Tag.create!(name: "friendship")
+    visit tags_path
+  end
+
   describe 'As A Visitor' do
     it 'displays all tags as links' do
-      @t1 = Tag.create!(name: "greetings")
-      @t2 = Tag.create!(name: "friendship")
-     
-      visit tags_path
-      
       expect(page).to have_link(@t1.name)
       expect(page).to have_link(@t2.name)
+
+      click_link "#{@t1.name}"
+
+      expect(current_path).to eq(tag_path(@t1))
     end
   end
 end
