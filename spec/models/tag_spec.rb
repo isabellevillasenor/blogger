8721ded06041807e17add72fb06b932b3cf625b4
Isabelle Villasenor <villasenori@outlook.com>
2020-12-30 11:24:44 -0700

    Add Model Method Spec for to_s

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -5,4 +5,14 @@ describe Tag do
     it { should have_many(:taggings) }
     it { should have_many(:articles).through(:taggings) }
   end
+
+  describe 'instance methods' do
+    it '#to_s' do
+      article = Article.create(title: "Tall Tables", body: "They are tough for the short legged")
+      article.tags.create(name: "furniture")
+      article.tags.create(name: "opinions")
+
+      expect(Tag.first.to_s).to eq("furniture")
+    end
+  end
 end
