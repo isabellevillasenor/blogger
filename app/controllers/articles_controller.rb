35b14bce3c0943eac7cf9ec5b9de5f72dbe39fac
Isabelle Villasenor <villasenori@outlook.com>
2020-12-30 17:26:22 -0700

    Add :image To article_params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -39,6 +39,6 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.permit(:title, :body, :tag_list, :image)
+    params.require[:article].permit(:title, :body, :tag_list, :image)
   end
 end
