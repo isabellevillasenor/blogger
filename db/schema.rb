6f527c0404c404988852551ac42dfe0c4c9ba58b
Isabelle Villasenor <villasenori@outlook.com>
2020-12-30 17:25:40 -0700

    Fix Column Name in Article DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_31_000217) do
+ActiveRecord::Schema.define(version: 2020_12_31_002407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2020_12_31_000217) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_file_name"
-    t.string "image_conent_type"
+    t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
   end
